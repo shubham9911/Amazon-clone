540835262b7e962ae92fcb84bb925e3494f631d3
shubham9911 <shubh9911am@gmail.com>
2021-10-07 16:40:57 +0530

    Updates

---
+++ precache-manifest.81e3952737708318e4cce173b3496aac.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9541e4497003f1a18ec4c7212dbb8c22",
+    "url": "/Amazon-clone/index.html"
+  },
+  {
+    "revision": "23a98bbde7c5a38485b5",
+    "url": "/Amazon-clone/static/css/main.6505a07d.chunk.css"
+  },
+  {
+    "revision": "989acb7301bd008947c0",
+    "url": "/Amazon-clone/static/js/2.310b54b8.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/Amazon-clone/static/js/2.310b54b8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23a98bbde7c5a38485b5",
+    "url": "/Amazon-clone/static/js/main.30014b0b.chunk.js"
+  },
+  {
+    "revision": "b4ca44839ab564cdfff3",
+    "url": "/Amazon-clone/static/js/runtime-main.a692d69b.js"
+  }
+]);
